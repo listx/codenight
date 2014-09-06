14edc24d3c579d878ad6207246f428307178eabd
Linus Arver <linusarver@gmail.com>
2014-09-06 00:29:44 -0700

    rpn.rb: raise error on unexpected operator application

--- rpn.rb
+++ rpn.rb
@@ -6,14 +6,18 @@ def evaluate(equation_str)
 	while terms.size > 0
 		term = terms.pop
 		case term
-		when '+', '-', '*'
-			b = stack.pop
-			a = stack.pop
-			op = term.to_sym
-			c = b.send(op, a)
-			stack.push(c)
 		when /^\d+?$/
 			stack.push(term.to_i)
+		when '+', '-', '*'
+			if stack.size < 2
+				raise "stack too small for operator application"
+			else
+				b = stack.pop
+				a = stack.pop
+				op = term.to_sym
+				c = b.send(op, a)
+				stack.push(c)
+			end
 		else
 			raise "invalid input `#{term}'"
 		end
